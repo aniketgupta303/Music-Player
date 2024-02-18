5376bfe41cfc9b49ffbd823f55d347ce162e9507
Aniket  Gupta <117922196+aniketgupta303@users.noreply.github.com>
2024-02-19 01:17:44 +0530

    Add files via upload

---
+++ Project music/music-list.js
@@ -0,0 +1,47 @@
+let allMusic = [
+
+{
+    name: "Hawa Banke",
+    artist: "Darshan Raval",
+    img: "music-1",
+    src: "music-1"
+},
+{
+    name: "Ye Pyar Nahi To Kya Hai",
+    artist: "Rahul Jain",
+    img: "music-2",
+    src: "music-2"
+},
+{
+    name: "Tera Naam",
+    artist: "Tulsi Kumar, Darshan Raval",
+    img: "music-3",
+    src: "music-3"
+},
+{
+    name: "Raataan Lambiyan",
+    artist: "Jubin Nautiyal, Asees Kaur",
+    img: "music-4",
+    src: "music-4"
+},
+{
+    name: "Asal Mein",
+    artist: "Darshan Raval",
+    img: "music-5",
+    src: "music-5"
+},
+{
+    name: "Rabba Mehr Kari",
+    artist: "Darshan Raval",
+    img: "music-6",
+    src: "music-6"
+},
+    
+{
+    name: "kya mohabbat hai",
+    artist: "Jubin Nautiyal, Tulsi Kumar",
+    img: "music-7",
+    src: "music-7"
+},
+
+];
