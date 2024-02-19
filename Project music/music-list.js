3f59f43d5ccd560f792af66bedda2cd3f71fdf52
Aniket  Gupta <117922196+aniketgupta303@users.noreply.github.com>
2024-02-19 10:53:42 +0530

    Update music-list.js

--- Project music/music-list.js
+++ Project music/music-list.js
@@ -3,45 +3,45 @@ let allMusic = [
 {
     name: "Hawa Banke",
     artist: "Darshan Raval",
-    img: "music-1",
-    src: "music-1"
+    img: "/Music-Player/Project music/music-1.jpg",
+    src: "/Music-Player/Project music/music-1.mp3"
 },
 {
     name: "Ye Pyar Nahi To Kya Hai",
     artist: "Rahul Jain",
-    img: "music-2",
-    src: "music-2"
+    img: "/Music-Player/Project music/music-2.jpg",
+    src: "/Music-Player/Project music/music-2.mp3"
 },
 {
     name: "Tera Naam",
     artist: "Tulsi Kumar, Darshan Raval",
-    img: "music-3",
-    src: "music-3"
+    img: "/Music-Player/Project music/music-3.jpg",
+    src: "/Music-Player/Project music/music-3.mp3"
 },
 {
     name: "Raataan Lambiyan",
     artist: "Jubin Nautiyal, Asees Kaur",
-    img: "music-4",
-    src: "music-4"
+    img: "/Music-Player/Project music/music-4.jpg",
+    src: "/Music-Player/Project music/music-4.mp3"
 },
 {
     name: "Asal Mein",
     artist: "Darshan Raval",
-    img: "music-5",
-    src: "music-5"
+    img: "/Music-Player/Project music/music-5.jpg",
+    src: "/Music-Player/Project music/music-5.mp3"
 },
 {
     name: "Rabba Mehr Kari",
     artist: "Darshan Raval",
-    img: "music-6",
-    src: "music-6"
+    img: "/Music-Player/Project music/music-6.jpg",
+    src: "/Music-Player/Project music/music-6.mp3"
 },
     
 {
     name: "kya mohabbat hai",
     artist: "Jubin Nautiyal, Tulsi Kumar",
-    img: "music-7",
-    src: "music-7"
+    img: "/Music-Player/Project music/music-7.jpg",
+    src: "/Music-Player/Project music/music-7.mp3"
 },
 
 ];
