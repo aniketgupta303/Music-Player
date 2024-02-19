0854edb4db4fbf35ef1a65710256300ed8c0344e
Aniket  Gupta <117922196+aniketgupta303@users.noreply.github.com>
2024-02-19 10:58:44 +0530

    music-list.js

--- Project music/music-list.js
+++ Project music/music-list.js
@@ -3,45 +3,45 @@ let allMusic = [
 {
     name: "Hawa Banke",
     artist: "Darshan Raval",
-    img: "/Music-Player/Project music/music-1.jpg",
-    src: "/Music-Player/Project music/music-1.mp3"
+    img: "/Music-Player/Project music/music-1",
+    src: "/Music-Player/Project music/music-1"
 },
 {
     name: "Ye Pyar Nahi To Kya Hai",
     artist: "Rahul Jain",
-    img: "/Music-Player/Project music/music-2.jpg",
-    src: "/Music-Player/Project music/music-2.mp3"
+    img: "/Music-Player/Project music/music-2",
+    src: "/Music-Player/Project music/music-2"
 },
 {
     name: "Tera Naam",
     artist: "Tulsi Kumar, Darshan Raval",
-    img: "/Music-Player/Project music/music-3.jpg",
-    src: "/Music-Player/Project music/music-3.mp3"
+    img: "/Music-Player/Project music/music-3",
+    src: "/Music-Player/Project music/music-3"
 },
 {
     name: "Raataan Lambiyan",
     artist: "Jubin Nautiyal, Asees Kaur",
-    img: "/Music-Player/Project music/music-4.jpg",
-    src: "/Music-Player/Project music/music-4.mp3"
+    img: "/Music-Player/Project music/music-4",
+    src: "/Music-Player/Project music/music-4"
 },
 {
     name: "Asal Mein",
     artist: "Darshan Raval",
-    img: "/Music-Player/Project music/music-5.jpg",
-    src: "/Music-Player/Project music/music-5.mp3"
+    img: "/Music-Player/Project music/music-5",
+    src: "/Music-Player/Project music/music-5"
 },
 {
     name: "Rabba Mehr Kari",
     artist: "Darshan Raval",
-    img: "/Music-Player/Project music/music-6.jpg",
-    src: "/Music-Player/Project music/music-6.mp3"
+    img: "/Music-Player/Project music/music-6",
+    src: "/Music-Player/Project music/music-6"
 },
     
 {
     name: "kya mohabbat hai",
     artist: "Jubin Nautiyal, Tulsi Kumar",
-    img: "/Music-Player/Project music/music-7.jpg",
-    src: "/Music-Player/Project music/music-7.mp3"
+    img: "/Music-Player/Project music/music-7",
+    src: "/Music-Player/Project music/music-7"
 },
 
 ];
